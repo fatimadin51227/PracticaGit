3ed7cc31d5df0454ba012fa1b96fce14a6560bb6
Fatima <fatima.din.51227@egibide.org>
2025-10-28 18:16:29 +0100

    Creación de las variables

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import javax.swing.*;
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Diez productos");
+        int contador = 0;
 
+        String descripcionMasLarga = "";
+        int longitudMasLarga = 0;
+
+        String descripcionMasUnidades = "";
+        int maxUnidades = -1;
+
+        String descripcionMasPronto = "";
+        LocalDate fechaMasPronto = null;
+        boolean salir = false;
     }
 }
